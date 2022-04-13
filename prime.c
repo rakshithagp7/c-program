73c76083739a9a54d68a291078f44919e2b24eb5
rakshithagp7 <102457960+rakshithagp7@users.noreply.github.com>
2022-04-13 11:57:29 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int u,l,i,j,count;
+	printf("enter the range to check for prime numbers: ");
+	scanf("%d%d",&l,&u);
+	printf("the prime numbers are:\n");
+	for(i=l;i<=u;i++){
+		count = 0;
+		for(j=1;j <=i;j++)
+		{
+			if(i%j==0)
+			{
+				count++;
+			}
+		}
+		if(count == 2){
+			printf("%d\t",i);
+		}
+	}
+	return 0;
+}
