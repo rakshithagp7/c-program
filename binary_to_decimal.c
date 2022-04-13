bd220b1e01d0f80691e380636d39419731545e1b
rakshithagp7 <102457960+rakshithagp7@users.noreply.github.com>
2022-04-13 20:14:39 +0530

    Update binary_to_decimal.c

--- binary_to_decimal.c
+++ binary_to_decimal.c
@@ -1,26 +1,21 @@
 #include<stdio.h>
-int btod(int); // Prototype of btod() function
+int btod(int);  
 int main()
 {
-int binary,decimal;
-// Accept input in binary format
-printf("Enter binary input :");
-scanf("%d",&binary);
-// Invoke btod() to convert binary to decimal
-decimal=btod(binary);
-// Print the decimal equivalent of binary
-printf("Decimal equivalent = %d\n", decimal);
-return 0;
+  int binary,decimal;
+  printf("Enter binary input :");
+  scanf("%d",&binary);
+  decimal=btod(binary);  
+  printf("Decimal equivalent = %d\n", decimal);
+  return 0;
 }
-// Function Definition of btod() function
 int btod(int bin)
-{
-if (bin==0)
-{
-return 0;
-}
+  if (bin==0)
+  {
+    return 0;
+  }
 else
 {
-return (bin%10 + btod(bin/10) * 2);
+  return (bin%10 + btod(bin/10) * 2);
 }
 }
