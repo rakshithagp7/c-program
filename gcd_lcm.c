fcb464f447fa5e8129e857358d7781b6fca5a3be
rakshithagp7 <102457960+rakshithagp7@users.noreply.github.com>
2022-04-13 20:31:23 +0530

    Update gcd_lcm.c

--- gcd_lcm.c
+++ gcd_lcm.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-int gcd(int x, int y); //function prototype
-int main()
+int main() 
+int gcd(int x, int y); 
 {
-int num1, num2, hcf, lcm;
-printf("Enter two integer Values:\n");
-scanf("%d %d", &num1, &num2);
-hcf = gcd(num1, num2);
-printf("GCD: %d", hcf);
-printf("\nLCM: %d", (num1 * num2) / hcf);
-return 0;
-}
-//recursive function
+  int num1, num2, hcf, lcm;
+  printf("Enter two integer Values:\n");
+  scanf("%d %d", &num1, &num2);
+  hcf = gcd(num1, num2);
+  printf("GCD: %d", hcf);
+  printf("\nLCM: %d", (num1 * num2) / hcf);
+  return 0;
+}  
 int gcd(int x, int y)
 {
-if (y == 0) //recursion termination condition
-{
-return x;
-}
-else
-{
-return gcd(y, x % y); //calls itself
-}
+  if (y == 0) //recursion termination condition
+  {
+    return x;
+  }
+  else
+  {
+    return gcd(y, x % y); //calls itself
+  }
 }
