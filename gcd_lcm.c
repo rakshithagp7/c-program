71b873625eae72ac26f2fa642676c8d24b27546f
rakshithagp7 <102457960+rakshithagp7@users.noreply.github.com>
2022-04-13 20:31:45 +0530

    Update gcd_lcm.c

--- gcd_lcm.c
+++ gcd_lcm.c
@@ -12,12 +12,12 @@ int gcd(int x, int y);
 }  
 int gcd(int x, int y)
 {
-  if (y == 0) //recursion termination condition
+  if (y == 0)  
   {
     return x;
   }
   else
   {
-    return gcd(y, x % y); //calls itself
+    return gcd(y, x % y);  
   }
 }
