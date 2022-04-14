096ca57bb5f02c7d54c05cf6a2cd84ee300cfd3b
rakshithagp7 <102457960+rakshithagp7@users.noreply.github.com>
2022-04-14 12:06:53 +0530

    Update talyor series.c

--- talyor series.c
+++ talyor series.c
@@ -3,31 +3,25 @@
 #define PI 3.142857
 int main()
 {
-float x,degree,nume,deno,sum,term;
-int i;
-/* Accept value of x in degree */
-printf("Enter degree:");
-scanf("%f",&degree);
-// Convert degree into radians
-x=degree*(PI/180.0);
-// Initialize values of sum, nume, deno and i variables
-sum=0;
-nume=x;
-deno=1.0;
-i=1;
-do
-{
-// compute term
-term=nume/deno;
-// Add term value to sum
-sum=sum+term;
-i=i+2;
-// compute next numerator and denominator values
-nume= -nume*x*x;
-deno=deno*i*(i-1);
-// printf("Term=%f\n",term);
-} while (fabs(term) >= 0.00001);
-printf("Computed value of Sin(%f)=%f\n",degree,sum);
-printf("Value from library function is sin(%f) = %f\n",degree,sin(x));
-return 0;
+  float x,degree,nume,deno,sum,term;
+  int i;
+  printf("Enter degree:");
+  scanf("%f",&degree);
+  x=degree*(PI/180.0);
+  sum=0;
+  nume=x;
+  deno=1.0;
+  i=1;
+  do
+  {  
+    term=nume/deno;
+    sum=sum+term;
+    i=i+2;
+    nume= -nume*x*x;
+    deno=deno*i*(i-1);
+  }
+  while (fabs(term) >= 0.00001);
+  printf("Computed value of Sin(%f)=%f\n",degree,sum);
+  printf("Value from library function is sin(%f) = %f\n",degree,sin(x));
+  return 0;
 }
